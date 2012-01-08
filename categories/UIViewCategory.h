380525022f32705a99ea2304c5cbce5597daad28
ettore <e@cubelogic.org>
2012-01-08 12:05:40 -0800

    added UIView category with basic getters/setters for frame

---
+++ categories/UIViewCategory.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2012, Cubelogic
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met:
+//
+// - Redistributions of source code must retain the above copyright notice, this 
+//   list of conditions and the following disclaimer.
+// - Redistributions in binary form must reproduce the above copyright notice, 
+//   this list of conditions and the following disclaimer in the documentation 
+//   and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+//  Created by ep on 1/8/12.
+//
+
+
+@interface UIView (Candygirl)
+
+-(CGFloat)x;
+-(void)setX:(CGFloat)x;
+-(CGFloat)y;
+-(void)setY:(CGFloat)y;
+-(CGFloat)w;
+-(void)setW:(CGFloat)w;
+-(CGFloat)h;
+-(void)setH:(CGFloat)h;
+-(CGFloat)right;
+-(CGFloat)bottom;
+
+@end
