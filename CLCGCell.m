ebd2fcf5daedfc20538d655057436078e7282128
ettore <e@cubelogic.org>
2013-04-11 15:47:37 -0700

    slightly shorter fade in for images

--- CLCGCell.m
+++ CLCGCell.m
@@ -191,7 +191,7 @@ static CGFloat sMaxAccessoryWidth = CLCG_DEFAULT_ACCESSORY_TYPE_W;
     // once the img view has been layed out once, no need to re-lay it out again
     [self setNeedsLayout]; //layout will happen in next update cycle
     CABasicAnimation *anim = [CABasicAnimation animationWithKeyPath:@"opacity"];
-    [anim setDuration:0.5]; // seconds
+    [anim setDuration:0.4]; // seconds
     [anim setFromValue:[NSNumber numberWithFloat:0.0]];
     [anim setToValue:[NSNumber numberWithFloat:1.0]];
     [layer addAnimation:anim forKey:@"animateOpacity"];
