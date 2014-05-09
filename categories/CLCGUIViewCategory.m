567b12994cfab6da08adf6cbf49f5d81d2047c6b
ettore <e@cubelogic.org>
2014-05-08 23:40:44 -0700

    reverting setX/Y/W/H to previous check-in: CGRectIntegral may cause unexpected changes in other coordinate or dimension

--- categories/CLCGUIViewCategory.m
+++ categories/CLCGUIViewCategory.m
@@ -1,20 +1,20 @@
 // Copyright (c) 2012, Cubelogic
 // All rights reserved.
 //
-// Redistribution and use in source and binary forms, with or without 
+// Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
 //
-// - Redistributions of source code must retain the above copyright notice, this 
+// - Redistributions of source code must retain the above copyright notice, this
 //   list of conditions and the following disclaimer.
-// - Redistributions in binary form must reproduce the above copyright notice, 
-//   this list of conditions and the following disclaimer in the documentation 
+// - Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
 //   and/or other materials provided with the distribution.
 //
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
 // CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
 // SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
 // INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
@@ -103,8 +103,8 @@
 -(void)setX:(CGFloat)x
 {
   CGRect r = [self frame];
-  r.origin.x = x;
-  [self setFrame:CGRectIntegral(r)];
+  r.origin.x = round(x);
+  [self setFrame:r];
 }
 
 
@@ -117,8 +117,8 @@
 -(void)setY:(CGFloat)y
 {
   CGRect r = [self frame];
-  r.origin.y = y;
-  [self setFrame:CGRectIntegral(r)];
+  r.origin.y = round(y);
+  [self setFrame:r];
 }
 
 
@@ -131,8 +131,8 @@
 -(void)setW:(CGFloat)w
 {
   CGRect r = [self frame];
-  r.size.width = w;
-  [self setFrame:CGRectIntegral(r)];
+  r.size.width = round(w);
+  [self setFrame:r];
 }
 
 
@@ -145,8 +145,8 @@
 -(void)setH:(CGFloat)h
 {
   CGRect r = [self frame];
-  r.size.height = h;
-  [self setFrame:CGRectIntegral(r)];
+  r.size.height = round(h);
+  [self setFrame:r];
 }
 
 
@@ -279,17 +279,17 @@
 -(UIView*)findFirstResponder
 {
   if ([self isFirstResponder]) {
-    return self;     
+    return self;
   }
-  
+
   for (UIView *subView in [self subviews]) {
     UIView *firstResponder = [subView findFirstResponder];
-    
+
     if (firstResponder != nil) {
       return firstResponder;
     }
   }
-  
+
   return nil;
 }
 
